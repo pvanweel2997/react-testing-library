b7317346c6d51b37259381fefbc94a5c6196a6e3
Patrick Van Weelden <pvanweel@yahoo.com>
2022-10-13 08:52:59 -0500

    added more tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import {  render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import { getUser } from './get-user'
-// import { mocked } from 'ts-jest'
+import { act } from 'react-dom/test-utils'
 
 jest.mock('./get-user')
 
-const mockGetUser = jest.mocked(getUser, {shallow: false});
+const mockGetUser = jest.mocked(getUser, {shallow: true});
 
 
 describe("When everything is OK",() => {
-  beforeEach(async() => {
-    render(<App />)
-    await waitFor(() => expect(mockGetUser).toHaveBeenCalled())
+  beforeEach( async () =>  {
+    await act(async () =>  { render(<App />) })
+    waitFor(() => expect(mockGetUser).toHaveBeenCalled())
   })
   test("should render the app component without crashing",() => {
-    // render(<App />)
     screen.debug()
   })
   test("should select the children that are being passed to the CustomInput component", () => {
-    // render(<App />)
     screen.getByText("Input:");
     expect(screen.getByText("Input:")).toBeInTheDocument()
     let error;
@@ -32,19 +30,16 @@ describe("When everything is OK",() => {
   })
 
   test("should select the input element by it's role", () => {
-    // render(<App />)
     screen.getByRole('textbox');
     expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 
   test("should select the label element by it's text", () => {
-    // render(<App />)
     screen.getAllByLabelText("Input:")
     
   })
 
   test("should select input label by placeholder text", () => {
-    // render(<App />)
     screen.getByPlaceholderText("Example")
   })
 
@@ -59,7 +54,20 @@ describe("When the component fetches the user successfully",()=> {
   })
 
   test("should call getUser once",async ()=> {
-   render(<App />)
-   await waitFor(() => expect(mockGetUser).toHaveBeenCalledTimes(1))
+    await act(async () =>  { render(<App />) })
+    act(() => expect(mockGetUser).toHaveBeenCalledTimes(1))
+  })
+
+  test("should render the username passed", async ()=> {
+    const name = 'John'
+    mockGetUser.mockImplementationOnce(() => 
+      Promise.resolve({id: '1', name})
+    )
+    render(<App />)
+    expect(screen.queryByText(/Username/)).toBeNull()
+    screen.debug()
+    expect(await screen.findByText(/Username/)).toBeInTheDocument()
+    screen.debug()
+    expect(await screen.findByText(/name/)).toBeInTheDocument()
   })
 })
